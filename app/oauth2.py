3cfea38fca078c08e8f48751abe912b2b06c7bbb
venz-x <venerable.zx@gmail.com>
2025-12-28 21:48:14 +0600

    login, token gen, validate done

--- app/oauth2.py
+++ app/oauth2.py
@@ -2,10 +2,64 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from . import database, models, schemas
+from .config import settings
+
+from datetime import datetime, timedelta, timezone
+import jwt
+from jwt import PyJWTError
 
 
-SECRET_KEY = "4497be80db150830c47c75b2c4b36f9dc37fe217"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    
+    to_encode.update({"exp": expire})
+    
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    
+    return encoded_jwt
+
+def verify_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id: str = payload.get("user_id")
+
+        if id is None:
+            raise credentials_exception
+        
+        return id
+
+    except PyJWTError:
+        raise credentials_exception
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(database.get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    token_id = verify_token(token, credentials_exception)
+
+    query = (select(models.User)
+                .where(models.User.id == token_id)
+    )
+
+    result = await db.execute(query)
+    user = result.scalar_one_or_none()
+
+    if user is None:
+        raise credentials_exception
+
+    return user
--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from .. import schemas, models, database
+from .. import schemas, models, database, utils, oauth2
 
 router = APIRouter(
     tags=["Authentication"]
@@ -13,3 +13,19 @@ async def login(user_credential: OAuth2PasswordRequestForm = Depends(), db: Asyn
     query = (select(models.User)
                 .where(models.User.email == user_credential.username)         
     )
+
+    result = await db.execute(query)
+    user = result.scalar_one_or_none()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credential")
+
+    if not utils.verify(user_credential.password, user.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credential")
+    
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
+
+    return {
+        "access_token": access_token, 
+        "token_type": "bearer"
+    }
