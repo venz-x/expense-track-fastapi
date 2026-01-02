f7cfe39eaed1981b33fe16f2c57b698017e4293c
venz-x <venerable.zx@gmail.com>
2026-01-02 21:00:11 +0600

    category get req added

--- app/routes/category.py
+++ app/routes/category.py
@@ -31,3 +31,29 @@ async def get_categories(db: AsyncSession = Depends(database.get_db), curent_use
     category_data = result.scalar_one_or_none()
 
     return category_data
+
+# all category and expenses
+@router.get('/category_expenses', response_model=list[schemas.CategoryWithExpenses])
+async def get_categories_expenses(db: AsyncSession = Depends(database.get_db), current_user: models.User = Depends(oauth2.get_current_user)):
+    query = (select(models.Category)
+                        .where(models.Category.owner_id == current_user.id)
+                        .options(selectinload(models.Category.expenses))
+    )
+
+    result = await db.execute(query)
+    category_expense_data = result.scalars().all()
+
+    return category_expense_data
+
+# specific category ad expenses
+@router.get("/category/{id}/expense", response_model=list[schemas.Expense])
+async def get_category_expenses(id: int, db: AsyncSession = Depends(database.get_db), current_user: models.User = Depends(oauth2.get_current_user)):
+    query = (select(models.Expense)
+                        .where(models.Expense.owner_id == current_user.id)
+                        .where(models.Expense.category_id == id)
+    )
+
+    result = await db.execute(query)
+    expenses = result.scalars().all()
+
+    return expenses
