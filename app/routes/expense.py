8bda0f0e333332abbacb749d4862082ac78009b0
venz-x <venerable.zx@gmail.com>
2026-01-04 13:40:11 +0600

    expense get, delete by id

--- app/routes/expense.py
+++ app/routes/expense.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy import select
 from sqlalchemy.orm import selectinload
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -31,3 +31,40 @@ async def get_all_expenses(db: AsyncSession = Depends(database.get_db), current_
     all_expenses = result.scalars().all()
 
     return all_expenses
+
+@router.get("/expense/{id}", response_model=schemas.Expense)
+async def delete_expense(id: int, db: AsyncSession = Depends(database.get_db), current_user: models.User = Depends(oauth2.get_current_user)):
+    query = (select(models.Expense)
+                        .where(models.Expense.id == id,
+                               models.Expense.owner_id == current_user.id
+                        )
+    )
+
+    result = await db.execute(query)
+    expense = result.scalar_one_or_none()
+
+    if expense is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+            detail=f"Expense with id {id} not found")
+
+    return expense
+
+@router.delete("/expense/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_expense(id: int, db: AsyncSession = Depends(database.get_db), current_user: models.User = Depends(oauth2.get_current_user)):
+    query = (select(models.Expense)
+                        .where(models.Expense.id == id,
+                               models.Expense.owner_id == current_user.id
+                        )
+    )
+
+    result = await db.execute(query)
+    expense_to_delete = result.scalar()
+
+    if expense_to_delete is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+            detail=f"Expense with id {id} not found")
+    
+    await db.delete(expense_to_delete)
+    await db.commit()
+
+    return None
